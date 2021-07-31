9b3be950f6a88ff7d731dee6a9c0000632dde21b
Leonardo Menezes <leonardo.lm0964@gmail.com>
2021-07-31 18:25:02 -0300

    Aula de matrizes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascript-class
+
+Repositório para aulas de Javascript. 
+
+Repositório original está no [Githud](https://github.com/Leozinho2006/javascript-class).
---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Aula sobre matrizes!");
+let matriz = [
+    'Hora',
+    "Minuto",
+    'Segundo'
+];
+
+console.log(matriz[0]);
+console.log("A matriz possui " + matriz.length + " elemento." );
+
+let criarObjeto = {};
+let obj = {
+    nome: "Leonardo",
+    idade: 15,
+    estaVelho() {
+        return true;
+    }
+};
+console.log(obj.nome + ' tem '+ obj.idade + ' anos de idade.');
+
+
+
